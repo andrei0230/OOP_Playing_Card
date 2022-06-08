a6325195338cbe9c5322c5a57e25ff528f1763c2
Andrei Ciucle <andrei.ciucle02@gmail.com>
2022-06-08 19:19:54 +0300

    Add Card class

---
+++ Card.cs
@@ -0,0 +1,31 @@
+﻿namespace OOP_Playing_Card
+{
+    public class Card
+    {
+        #region Fields
+
+        private Suit _suit;
+
+        private Rank _rank;
+
+        #endregion
+
+        #region Constructors
+        
+        public Card(Suit suit, Rank rank)
+        {
+            _suit = suit;
+            _rank = rank;
+        }
+
+        #endregion
+
+        #region Properties
+
+        public Suit Suit { get { return _suit; } }
+
+        public Rank Rank { get { return _rank; } } 
+
+        #endregion
+    }
+}
