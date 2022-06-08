7d45b5fd1fbf45f37abc1f18c65a0b7556da916a
Andrei Ciucle <andrei.ciucle02@gmail.com>
2022-06-08 19:19:23 +0300

    Add Rank enumeration

---
+++ Rank.cs
@@ -0,0 +1,19 @@
+﻿namespace OOP_Playing_Card
+{
+    public enum Rank
+    {
+        two = 2,
+        three,
+        four,
+        five,
+        six,
+        seven,
+        eight,
+        nine,
+        ten,
+        Jack = 10,
+        Queen = 10,
+        King = 10,
+        Ace = 11,
+    }
+}
